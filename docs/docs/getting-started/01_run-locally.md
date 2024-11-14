49e6f7c1cbf0e8af95097080caee5aa0c2f3ccf7
Deepak Jayanna <88205314+djayanna@users.noreply.github.com>
2024-11-14 14:08:02 -0500

    merging chages from parent

--- docs/docs/getting-started/01_run-locally.md
+++ docs/docs/getting-started/01_run-locally.md
@@ -9,11 +9,12 @@ If you wish to modify the template in any way, you will need to set up your deve
 
 - you are running node v18 or above
 - twilio cli 5.20.0 or above is [installed](https://www.twilio.com/docs/twilio-cli/getting-started/install) (`twilio --version`)
-- twilio flex plugins 7.0.0 or above is [installed](https://www.twilio.com/docs/flex/developer/plugins/cli/install#install-the-flex-plugins-cli) (`twilio plugins`, `twilio plugins:install @twilio-labs/plugin-flex@latest`)
+- twilio flex plugins 7.0.5 or above is [installed](https://www.twilio.com/docs/flex/developer/plugins/cli/install#install-the-flex-plugins-cli) (`twilio plugins`, `twilio plugins:install @twilio-labs/plugin-flex@latest`)
 - twilio serverless plugin 3.1.6 or above is [installed](https://www.twilio.com/docs/labs/serverless-toolkit/getting-started#install-the-twilio-serverless-toolkit) (`twilio plugins` `twilio plugins:install @twilio-labs/plugin-serverless@latest`)
 - `twilio profiles:list` has an active account set.
 - have the twilio auth token for your account ready (you can find this in the [Twilio Console](https://console.twilio.com/))
 - If you are running Windows, ensure you are using Git Bash to execute the commands when working with the template, rather than Command Prompt or PowerShell.
+  - We also need to instruct `npm` to use Git Bash for executing scripts. To do so, run: `npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"` (adjust as needed for the installed Git location)
 
 :::caution Feature Functionality
  some features may not be functional without a deployment of the TaskRouter configuration and studio flows. It is recommended to do at least one initial deploy to your environment using the [Deploy Flex GitHub workflow](install-template)
--- plugin-flex-ts-template-v2/src/feature-library/custom-transfer-directory/flex-hooks/custom-actions/startExternalColdTransfer.ts
+++ plugin-flex-ts-template-v2/src/feature-library/custom-transfer-directory/flex-hooks/custom-actions/startExternalColdTransfer.ts
@@ -1,9 +1,10 @@
-import { Actions, ITask, Notifications, TaskHelper } from '@twilio/flex-ui';
+import { Actions, ITask, Notifications, TaskHelper, templates } from '@twilio/flex-ui';
 
 import { shouldSkipPhoneNumberValidation } from '../../config';
 import PhoneNumberService from '../../../../utils/serverless/PhoneNumbers/PhoneNumberService';
 import ProgrammableVoiceService from '../../../../utils/serverless/ProgrammableVoice/ProgrammableVoiceService';
 import { CustomTransferDirectoryNotification } from '../notifications/CustomTransferDirectory';
+import { StringTemplates } from '../strings/CustomTransferDirectory';
 import logger from '../../../../utils/logger';
 
 export const registerStartExternalColdTransfer = async () => {
@@ -29,8 +30,15 @@ export const registerStartExternalColdTransfer = async () => {
         if (!validationCheck.success) {
           Notifications.showNotification(CustomTransferDirectoryNotification.PhoneNumberFailedValidationCheckRequest);
           return;
-        } else if (validationCheck.success && !validationCheck.lookupResponse?.valid) {
-          const errors = validationCheck.lookupResponse.validationErrors.join(', ');
+        } else if (validationCheck.success && !validationCheck.valid) {
+          let errors = validationCheck.invalidReason;
+
+          errors = errors?.replace('COUNTRY_DISABLED', templates[StringTemplates.CountryDisabled]());
+          errors = errors?.replace(
+            'HIGH_RISK_SPECIAL_NUMBER_DISABLED',
+            templates[StringTemplates.HighRiskSpecialNumberDisabled](),
+          );
+
           Notifications.showNotification(
             CustomTransferDirectoryNotification.PhoneNumberFailedValidationCheckWithErrors,
             {
@@ -44,7 +52,7 @@ export const registerStartExternalColdTransfer = async () => {
 
       try {
         await ProgrammableVoiceService.startColdTransfer(
-          task?.attributes?.call_sid ?? task.attributes.conference.participants.customer,
+          task?.attributes?.conference?.participants?.customer ?? task?.attributes?.call_sid,
           phoneNumber,
           callerId,
         );
